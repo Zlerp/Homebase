d5ebb4c1463d0ae4a0a0b3c3b57e7e2008e06958
Zlerp <zhpdeals@gmail.com>
2015-07-06 16:17:05 -0400

    working social fonts

--- server.rb
+++ server.rb
@@ -3,3 +3,7 @@ require 'sinatra'
 get '/' do
   File.read(File.join('public', 'Homebase.html')) # YOU NEED THIS!!!!!!
 end
+
+# get '/' do
+# 	File.read("#{settings.public_folder}/index.html")
+# end
